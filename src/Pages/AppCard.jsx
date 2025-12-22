5431ff205764cf818b1a6505dfed4596a3afdf9f
ShovonTheBarua <shovonbarua0186@gmail.com>
2025-12-22 22:21:21 +0600

    Home page added

--- src/Pages/AppCard.jsx
+++ src/Pages/AppCard.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { FaStar } from "react-icons/fa";
 
 const AppCard = ({ data }) => {
   const { title, companyName, image } = data;
   return (
-    <div>
-      <img className="" src={image} alt="" />
-      <div>
-        <h3>{title}</h3>
-        <span>
-          <button>9M</button>
-          <button>5</button>
+    <div className="p-4 shadow-2xl ">
+      <div className="flex">
+        <img
+          className="rounded-lg mx-auto h-68 object-cover"
+          src={image}
+          alt=""
+        />
+      </div>
+      <div className="space-y-2">
+        <h3 className="text-xl font-medium">{title}</h3>
+        <span className="flex justify-between">
+          <button className="flex gap-2 p-2 items-center rounded-md text-[#00D390] bg-[#F1F5E8]">
+            <MdOutlineFileDownload />
+            9M
+          </button>
+          <button className="flex gap-2 p-2 items-center rounded-md text-[#FF8811] bg-[#FFF0E1]">
+            <FaStar />5
+          </button>
         </span>
       </div>
     </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,96 @@
 import React from "react";
 import useAppData from "../Hooks/useAppData";
 import AppCard from "./AppCard";
+import { Link } from "react-router";
+import appStore from "../../assets/appStore.png";
+import playStore from "../../assets/googlePlay.png";
+import hero from "../../assets/hero.png";
 
 const Home = () => {
   const { appData } = useAppData();
 
-  const trendingData = appData.slice(0, 27);
+  const trendingData = appData.slice(0, 8);
   return (
     <div>
-      <h1>Trending Apps</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mx-auto max-w-7xl">
-        {trendingData.map((data) => (
-          <AppCard data={data} />
-        ))}
+      {/* Title */}
+      <div className="text-center px-2 mt-20">
+        <h1 className="text-[#001931] text-7xl font-bold mb-4 ">
+          We Build <br />
+          <span className="text-transparent bg-clip-text bg-linear-to-r from-[#632EE3] to-[#9F62F2]">
+            Productive
+          </span>{" "}
+          Apps
+        </h1>
+        <p className="text-gray-600">
+          AtHERO.IO, we craft innovative apps designed to make everyday life
+          simpler, smarter, and more exciting. <br />
+          Our goal is to turn your ideas into digital experiences that truly
+          make an impact.
+        </p>
+      </div>
+      {/* button */}
+      <div className="flex justify-center gap-3 my-14 px-3">
+        <Link
+          to="https://play.google.com/store/games?hl=en"
+          className="flex items-center border border-gray-300 rounded-lg px-4 py-1"
+        >
+          <img src={playStore} alt="" />
+          <p className="text-xl font-semibold pl-1">Google Play</p>
+        </Link>
+        <Link
+          to="https://www.apple.com/app-store/"
+          className="flex items-center border border-gray-300 rounded-lg px-4 py-1"
+        >
+          <img src={appStore} alt="" />
+          <p className="text-xl font-semibold pl-1">App Store</p>
+        </Link>
+      </div>
+
+      <img src={hero} className="mx-auto px-2" alt="" />
+
+      {/* state cards  */}
+      <div className="text-center px-2  py-20 text-white bg-linear-to-r from-[#632EE3] to-[#9F62F2]">
+        <h1 className="text-5xl font-bold mb-10">
+          Trusted by Millions, Built for You
+        </h1>
+        <div className="grid mx-auto max-w-7xl grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="text-center space-y-4">
+            <p>Total Downloads</p>
+            <h1 className="text-6xl font-bold">29.6M</h1>
+            <p>21% more than last month</p>
+          </div>
+          <div className="text-center space-y-4">
+            <p>Total Reviews</p>
+            <h1 className="text-6xl font-bold">906K</h1>
+            <p>46% more than last month</p>
+          </div>
+          <div className="text-center space-y-4">
+            <p>Active Apps</p>
+            <h1 className="text-6xl font-bold">132+</h1>
+            <p>31 more will Launch</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Trending section */}
+      <div className="my-20 px-3">
+        <div className="text-center ">
+          <h1 className="text-5xl font-bold mb-4">Trending Apps</h1>
+          <p className="text-gray-600">
+            Explore All Trending Apps on the Market developed by us
+          </p>
+        </div>
+        <div className="grid gap-4 my-10  grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mx-auto max-w-7xl">
+          {trendingData.map((data) => (
+            <AppCard data={data} />
+          ))}
+        </div>
+        <Link
+          to="/apps"
+          className="block w-fit mx-auto px-8 py-4 rounded-lg text-white font-semibold bg-linear-to-r from-[#632EE3] to-[#9F62F2]"
+        >
+          Show All
+        </Link>
       </div>
     </div>
   );
